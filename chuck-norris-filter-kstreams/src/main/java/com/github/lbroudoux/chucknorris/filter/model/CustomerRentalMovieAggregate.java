801be6c823409cf79e1d7ef6ba696351ce2fe05f
lbroudoux <laurent.broudoux@gmail.com>
2019-05-15 16:09:43 +0200

    Updating kstreams stuffs

---
+++ chuck-norris-filter-kstreams/src/main/java/com/github/lbroudoux/chucknorris/filter/model/CustomerRentalMovieAggregate.java
@@ -0,0 +1,53 @@
+package com.github.lbroudoux.chucknorris.filter.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * @author laurent
+ */
+public class CustomerRentalMovieAggregate {
+
+   @JsonProperty("movie")
+   private final Movie movie;
+
+   @JsonProperty("customer")
+   private Customer customer;
+
+   @JsonProperty("rental")
+   private final Rental rental;
+
+
+   @JsonCreator
+   public CustomerRentalMovieAggregate(
+         @JsonProperty("rental") Rental rental,
+         @JsonProperty("movie") Movie movie) {
+      this.rental = rental;
+      this.movie = movie;
+   }
+
+   public Movie getMovie() {
+      return movie;
+   }
+
+   public Customer getCustomer() {
+      return customer;
+   }
+
+   public void setCustomer(Customer customer) {
+      this.customer = customer;
+   }
+
+   public Rental getRental() {
+      return rental;
+   }
+
+   @Override
+   public String toString() {
+      return "CustomerRentalMovieAggregate{" +
+            "rental=" + rental +
+            ", movie=" + movie +
+            ", customer=" + customer +
+            "}";
+   }
+}
