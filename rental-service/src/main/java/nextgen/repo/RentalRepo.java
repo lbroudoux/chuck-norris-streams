59f69c82f5e0832d7193bb121df478392dde5c66
alainpham <alainphm@gmail.com>
2019-05-16 16:02:20 +0200

    Rental service and app is done

--- rental-service/src/main/java/nextgen/repo/RentalRepo.java
+++ rental-service/src/main/java/nextgen/repo/RentalRepo.java
@@ -1,9 +1,11 @@
 package nextgen.repo;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import nextgen.model.Rental;
 
 public interface RentalRepo extends PagingAndSortingRepository<Rental, Long>{
 
+	Iterable<Rental> findByCustomerId(@Param(value = "id")Long id);
 }
--- rental-service/src/main/resources/sql/examples.sql
+++ rental-service/src/main/resources/sql/examples.sql
@@ -1,13 +1,29 @@
+INSERT INTO movie (main_actor,title,`year`) VALUES (
+'Jessica Lange','King Kong',1976);
+INSERT INTO movie (main_actor,title,`year`) VALUES (
+'Chuck Norris','Delta Force',1986);
+INSERT INTO movie (main_actor,title,`year`) VALUES (
+'Anthony Hopkins','The Elephant Man',1980);
+INSERT INTO movie (main_actor,title,`year`) VALUES (
+'Chuck Norris','Invasion U.S.A.',1985);
+INSERT INTO movie (main_actor,title,`year`) VALUES (
+'Chuck Norris','An Eye for an Eye',1981);
+INSERT INTO movie (main_actor,title,`year`) VALUES (
+'Roger Miller','Robin Hood',1973);
+INSERT INTO movie (main_actor,title,`year`) VALUES (
+'Charles Chaplin','Charly Chaplin in Wien',1931);
+INSERT INTO movie (main_actor,title,`year`) VALUES (
+'Chuck Norris','The Octagon',1980);
+INSERT INTO movie (main_actor,title,`year`) VALUES (
+'James Stewart','Vertigo',1958);
+
+
 INSERT INTO customer
-(id, first_name, last_name, twitter_handle)
-VALUES(nextval('customer_id_seq'::regclass), 'alain', 'pham', '@koint');
+( first_name, last_name, twitter_handle)
+VALUES( 'alain', 'pham', '@koint');
+INSERT INTO customer
+( first_name, last_name, twitter_handle)
+VALUES( 'laurent', 'broudoux', '@lbroudoux');
 
-INSERT INTO movie
-(id, main_actor, title, "year")
-VALUES(nextval('movie_id_seq'::regclass), 'chuck', 'texas ranger', 0);
-
-INSERT INTO rental
-(id, rental_duration, start_date, customer_id, movie_id)
-VALUES(nextval('rental_id_seq'::regclass), 7, '2019/05/10', 1, 1);
 
 commit;
