be35fc5003047ccf2a69ca045db7e47a9bb6067a
alainpham <alainphm@gmail.com>
2019-05-17 17:38:23 +0200

    chuck facts project

---
+++ chuck-norris-facts-api/src/main/java/nexgen/RestConfigurator.java
@@ -0,0 +1,31 @@
+package nexgen;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RestConfigurator extends RouteBuilder {
+
+	@Autowired
+	Environment environment;
+
+	@Override
+	public void configure() throws Exception {
+		restConfiguration()
+		.component("servlet")
+		.bindingMode(RestBindingMode.json)
+		.contextPath(environment.getProperty("camelrest.contextPath"))
+		.port(environment.getProperty("camelrest.port"))
+		.apiContextPath("/api-docs")
+		.apiProperty("cors", "true")
+		.apiProperty("api.title", environment.getProperty("camel.springboot.name"))
+		.apiProperty("api.version", environment.getProperty("camelrest.apiversion"))
+		.host(environment.getProperty("camelrest.host"))
+		.dataFormatProperty("prettyPrint", "true");
+	}
+
+
+}
